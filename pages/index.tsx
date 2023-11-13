321ab1b51cba2d471ed6b3fe0b2217719a212218
Mirsaid25 <siddikovmirsaid01@gmail.com>
2023-11-13 17:08:19 +0500

    add

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,19 +19,22 @@ export default function Home() {
             <Header />
             <main>
                 <section className="section_1">
-                    <div className="bg-[url('/section1/section1_bg.jpg')] bg-center h-[1000px] max-xl:h-[800px] max-lg:h-[650px] max-md:h-[550px] max-sm:h-[400px] bg-no-repeat bg-cover flex flex-col items-center pt-[300px] max-xl:pt-[200px] max-md:pt-[150px] max-sm:pt-[120px] relative">
-                        <p className="text-4xl max-xl:text-3xl max-lg:text-2xl max-sm:text-xl text-white">
+                    <div className="rounded-b-[80px] max-xl:rounded-b-[70px] max-lg:rounded-b-[50px] max-md:rounded-b-[40px] max-sm:rounded-b-[30px] absolute top-0 left-0 z-[1] bg-[#0000ff3e] w-full h-[1000px] max-xl:h-[800px] max-lg:h-[650px] max-md:h-[550px] max-sm:h-[400px]">
+
+                    </div>
+                    <div className="rounded-b-[80px] max-xl:rounded-b-[70px] max-lg:rounded-b-[50px] max-md:rounded-b-[40px] max-sm:rounded-b-[30px] bg-[url('/images/32.JPG')] bg-center h-[1000px] max-xl:h-[800px] max-lg:h-[650px] max-md:h-[550px] max-sm:h-[400px] bg-no-repeat bg-cover flex flex-col items-center pt-[300px] max-xl:pt-[200px] max-md:pt-[150px] max-sm:pt-[120px] relative">
+                        <p className="z-10 text-4xl max-xl:text-3xl max-lg:text-2xl max-sm:text-xl text-white">
                             welcome to School
                         </p>
-                        <h1 className="text-7xl  max-xl:text-6xl max-lg:text-5xl max-sm:text-4xl max-[400px]:text-3xl text-white text-center mt-5 max-sm:mt-2">
+                        <h1 className="z-10 text-7xl  max-xl:text-6xl max-lg:text-5xl max-sm:text-4xl max-[400px]:text-3xl text-white text-center mt-5 max-sm:mt-2">
                             Education is the best <br /> key to success in life
                         </h1>
-                        <button className="bg-white flex items-center gap-4 px-[28px] py-[16px] max-lg:px-5 max-lg:py-3 max-sm:px-2 max-sm:py-1 text-[#032D6C] text-[18px] max-sm:text-base max-[400px]:text-sm font-medium rounded-md mt-10  max-sm:mt-10">
+                        <button className="z-10 bg-white flex items-center gap-4 px-[28px] py-[16px] max-lg:px-5 max-lg:py-3 max-sm:px-2 max-sm:py-1 text-[#032D6C] text-[18px] max-sm:text-base max-[400px]:text-sm font-medium rounded-md mt-10  max-sm:mt-10">
                             UPCOMING NOTICE
                             <BiChevronRight size={30} color={"#032D6C"} />
                         </button>
 
-                        <div className="flex items-center justify-between gap-10 absolute -bottom-[100px] max-lg:hidden">
+                        <div className="z-10 flex items-center justify-between gap-10 absolute -bottom-[100px] max-lg:hidden">
                             <div className="p-10 max-xl:p-7 bg-white flex flex-col justify-center items-center gap-5 rounded-[36px]">
                                 <Image
                                     src={"/section1/section1_bottom1.png"}
@@ -276,7 +279,7 @@ export default function Home() {
                     <p className="text-[#032D6C] text-4xl max-xl:text-3xl max-[400px]:text-3xl font-bold text-center">
                         University Video Tour
                     </p>
-                    <div className="mt-20 max-xl:mt-16 max-lg:mt-10 h-[700px] max-xl:h-[550xp] max-lg:h-[450px] max-md:h-[350px] max-sm:h-[250px] max-[400px]:h-[200px] bg-[url('/section4/section4.jpg')] bg-cover bg-no-repeat bg-center"></div>
+                    <div className="mt-20 max-xl:mt-16 max-lg:mt-10 h-[700px] max-xl:h-[550xp] max-lg:h-[450px] max-md:h-[350px] max-sm:h-[250px] max-[400px]:h-[200px] bg-[url('/images/33.JPG')] bg-cover bg-no-repeat bg-center"></div>
                 </section>
 
                 <section className="section_5 mt-20 max-xl:mt-10">
@@ -374,7 +377,7 @@ export default function Home() {
                 <section className="section_7 mt-20 max-xl:mt-10">
                     <div className="custom_container">
                         <p className="text-[#032D6C] text-4xl max-xl:text-3xl font-bold text-center">
-                            Our Expert Teachers
+                            Frame by Frame
                         </p>
                         <p className="mt-10 max-md:mt-5 text-[#032D6C] text-center">
                             Explore a vibrant collection of visual masterpieces,
@@ -394,30 +397,30 @@ export default function Home() {
                             <div className="not_active">Curiculum</div>
                         </div>
 
-                        <div className="mt-10 max-md:mt-5 grid grid-cols-4 auto-rows-[180px] max-xl:auto-rows-[160px] max-lg:auto-rows-[140px] max-md:auto-rows-[100px] max-sm:auto-rows-[70px]  max-[400px]:auto-rows-[60px] gap-5 max-md:gap-2">
-                            <div className="bg-[royalblue] col-start-1 col-end-2 row-start-1 row-end-3">
-                                1
+                        <div className="mt-10 max-md:mt-5 grid grid-cols-4 auto-rows-[250px] max-xl:auto-rows-[160px] max-lg:auto-rows-[140px] max-md:auto-rows-[100px] max-sm:auto-rows-[70px]  max-[400px]:auto-rows-[60px] gap-5 max-md:gap-2">
+                            <div className="bg-[url('/images/1.JPG')] bg-center bg-cover bg-no-repeat col-start-1 col-end-2 row-start-1 row-end-3">
+                                
                             </div>
-                            <div className="bg-[#8941e1] col-start-2 col-end-4 row-start-1 row-end-2">
-                                2
+                            <div className="bg-[url('/images/2.JPG')] bg-center bg-cover bg-no-repeat col-start-2 col-end-4 row-start-1 row-end-2">
+                                
                             </div>
-                            <div className="bg-[#eae382] col-start-4 col-end-5 row-start-1 row-end-2">
-                                3
+                            <div className="bg-[url('/images/3.JPG')] bg-center bg-cover bg-no-repeat col-start-4 col-end-5 row-start-1 row-end-2">
+                                
                             </div>
-                            <div className="bg-[#ea8282] col-start-1 col-end-2 row-start-3 row-end-5">
-                                4
+                            <div className="bg-[url('/images/4.JPG')] bg-center bg-cover bg-no-repeat col-start-1 col-end-2 row-start-3 row-end-5">
+                                
                             </div>
-                            <div className="bg-[royalblue] col-start-2 col-end-4 row-start-2 row-end-5">
-                                5
+                            <div className="bg-[url('/images/32.JPG')] bg-center bg-cover bg-no-repeat col-start-2 col-end-4 row-start-2 row-end-5">
+                                
                             </div>
-                            <div className="bg-[#8941e1] col-start-4 col-end-5 row-start-2 row-end-4">
-                                6
+                            <div className="bg-[url('/images/9.JPG')] bg-center bg-cover bg-no-repeat col-start-4 col-end-5 row-start-2 row-end-4">
+                                
                             </div>
-                            <div className="bg-[#eae382] col-start-1 col-end-4 row-start-5 row-end-6">
-                                7
+                            <div className="bg-[url('/images/18.JPG')] bg-center bg-cover bg-no-repeat col-start-1 col-end-4 row-start-5 row-end-6">
+                                
                             </div>
-                            <div className="bg-[#ea8282] col-start-4 col-end-5 row-start-4 row-end-6">
-                                8
+                            <div className="bg-[url('/images/25.JPG')] bg-center bg-cover bg-no-repeat col-start-4 col-end-5 row-start-4 row-end-6">
+                                
                             </div>
                         </div>
                     </div>
@@ -493,19 +496,23 @@ export default function Home() {
                         Keep up our latest news and events,Subscribe our
                         newsletter
                     </p>
-                    <form className="flex items-center gap-3 w-[70%] max-sm:w-[90%] mt-5" action="">
-                        <input type="text" placeholder="Email Address" className="border font-medium border-[#032D6C] rounded-md px-5 py-3 max-sm:text-sm max-sm:px-3 max-sm:py-2 bg-[#E6EDF8] w-full" />
-                        <button className="bg-[#032D6C] text-center text-[#E6EDF8] max-sm:text-sm px-5 py-3 max-sm:px-3 max-sm:py-2 rounded-md">Subscribe</button>
+                    <form
+                        className="flex items-center gap-3 w-[70%] max-sm:w-[90%] mt-5"
+                        action=""
+                    >
+                        <input
+                            type="text"
+                            placeholder="Email Address"
+                            className="border font-medium border-[#032D6C] rounded-md px-5 py-3 max-sm:text-sm max-sm:px-3 max-sm:py-2 bg-[#E6EDF8] w-full"
+                        />
+                        <button className="bg-[#032D6C] text-center text-[#E6EDF8] max-sm:text-sm px-5 py-3 max-sm:px-3 max-sm:py-2 rounded-md">
+                            Subscribe
+                        </button>
                     </form>
                 </div>
                 <div className="custom_container flex max-xl:flex-col items-start max-xl:items-center justify-center gap-16 max-sm:gap-7 pb-16 max-sm:pb-7">
                     <div className="max-xl:flex max-xl:flex-col max-xl:items-center">
-                        <Image
-                            src="/logo.png"
-                            width={190}
-                            height={50}
-                            alt=""
-                        />
+                        <Image src="/logo.png" width={190} height={50} alt="" />
                         <p className="text-white mt-5 leading-6 w-[217px] max-xl:w-[500px] max-sm:w-full max-xl:text-center">
                             Lorem ipsum dolor sit amet, consectetuer adipiscing
                             elit. Maecenas porttitor congue massa. Fusce
@@ -597,9 +604,11 @@ export default function Home() {
                     </div>
                 </div>
 
-                <hr className="w-full border border-white max-lg:hidden"/>
+                <hr className="w-full border border-white max-lg:hidden" />
 
-                <p className="text-center text-white py-5 max-sm:py-2 max-sm:text-xs">copyright@school2023all right reserved</p>
+                <p className="text-center text-white py-5 max-sm:py-2 max-sm:text-xs">
+                    copyright@school2023all right reserved
+                </p>
             </footer>
         </>
     );
