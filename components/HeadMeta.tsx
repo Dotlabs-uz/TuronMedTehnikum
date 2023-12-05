73d0d8dbabf94f7326203ebfd8b47b5b2724ea51
Mirsaid25 <siddikovmirsaid01@gmail.com>
2023-12-06 02:50:39 +0500

    add meta

--- components/HeadMeta.tsx
+++ components/HeadMeta.tsx
@@ -7,15 +7,11 @@ const HeadMeta: React.FC<IHeadMeta> = () => {
     return (
         <Head>
             <title>Турон медицинский техникум</title>
-            <meta
-                name="description"
-                content="Официальный сайт Турон медицинского техникума. Информация о программах обучения и событиях в учебном заведении."
-            />
-            <meta
-                name="keywords"
-                content="Турон, медицинский техникум, образование, студенты, программы обучения, Самарканд"
-            />
-            <meta http-equiv="Content-Language" content="ru" />
+            <meta name="description" content="Официальный сайт Турон медицинского техникума в Самарканде. Информация о программах обучения и событиях в учебном заведении."/>
+            <meta name="keywords" content="Турон, медицинский техникум, образование, студенты, программы обучения"/>
+            <meta name="description" content="Турон тиббий техникуми расмий веб-сайти. Таълим дастурлари ва муассасалари ҳақида маълумот."/>
+            <meta name="keywords" content="Turon, Turon med texnikumi, Samarqand med texnikumi,texnikum, med" />
+            <meta httpEquiv="Content-Language" content="uz" />
             <meta charSet="UTF-8" />
             <meta
                 name="viewport"
@@ -29,7 +25,10 @@ const HeadMeta: React.FC<IHeadMeta> = () => {
             <link rel="icon" type="image/png" href="/logo.png" />
             <meta property="og:title" content="Turon med texnikum" />
             <meta property="og:type" content="website" />
-            <meta property="og:description" content="Турон медицинский техникум" />
+            <meta
+                property="og:description"
+                content="Турон медицинский техникум"
+            />
             <meta
                 property="og:url"
                 content="https://resplendent-zuccutto-be76bf.netlify.app/"
